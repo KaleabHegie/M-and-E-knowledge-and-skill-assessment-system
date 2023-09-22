df90b8f92f1bcdd5814023d468a42cf96dd85c4e
Betel Tesfu <116803946+Bethel16@users.noreply.github.com>
2023-09-22 15:15:32 -0700

    user_profile urls

--- survey_managment/urls.py
+++ survey_managment/urls.py
@@ -4,4 +4,7 @@ from .import views
 urlpatterns = [
     path('',views.indexView, name='Index'),
     path('surveyCreation/',views.indexView, name='SurveyCreation'),
+    path('user_profile/',views.user_profile, name='user_profile'),
+    
+
 ]
