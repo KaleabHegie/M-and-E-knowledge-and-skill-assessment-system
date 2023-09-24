bc2a1c23df318c0e2de44bc3473d235e8265eaea
Betel Tesfu <116803946+Bethel16@users.noreply.github.com>
2023-09-24 16:33:50 -0700

    change password

--- survey_managment/urls.py
+++ survey_managment/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.indexView, name='Index'),
     path('surveyCreation/',views.surveyCreationView, name='SurveyCreation'),
     path('user_profile/',views.user_profile, name='user_profile'),
+    path('change_password/',views.change_password, name='change_password'),
     path('ChartAnalysis/',views.Mychartanalysis, name='ChartAnalysis'),
     
 
