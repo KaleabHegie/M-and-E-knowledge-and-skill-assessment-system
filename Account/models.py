530d82202a052aa33ba0916cd507eba48c99fa00
Benjii400 <benjiyg400@gmail.com>
2023-09-25 13:04:34 +0300

    modelUpdate

--- Account/models.py
+++ Account/models.py
@@ -15,3 +15,4 @@ class System_User(models.Model):
     phone_number = models.CharField( max_length=50)    
     image = models.ImageField(upload_to='')
     custom_user = models.ForeignKey(CustomUser , on_delete=models.CASCADE)
+    date_of_birth = models.DateField( auto_now=False, auto_now_add=False , null=True)
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Password changed Succesussfuly')
+            return redirect('/')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'survey_managment/change_password.html', {'form': form})
+
+
