986bfc10f4cd2bd2e0336eff6ac89c9f1562186b
Betel Tesfu <116803946+Bethel16@users.noreply.github.com>
2023-09-25 11:06:22 -0700

    urls

--- survey_managment/urls.py
+++ survey_managment/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('surveyCreation/',views.surveyCreationView, name='SurveyCreation'),
     path('user_profile/',views.user_profile, name='user_profile'),
     path('ChartAnalysis/',views.Mychartanalysis, name='ChartAnalysis'),
+    path('change_password', views.change_password,name='change_password')
     
 
 ]
