664e5c4a04a22cda20ce9a87cb78d62731f023d1
Feven-shi <fevenshibeshi@gmail.com>
2023-09-27 10:29:36 +0300

    login url modified and forgot password template

--- survey_managment/urls.py
+++ survey_managment/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('user_registration', views.user_registration,name='user_registration'),
     path('user_profile/',views.user_profile, name='user_profile'),
     path('ChartAnalysis/',views.Mychartanalysis, name='ChartAnalysis'),
-    path('change_password', views.change_password,name='change_password')
+    path('change_password', views.change_password,name='change_password'),
+    path('forgotPassword/', views.forgotPasswordView,name='forgotPassword'),
+    path('login/', views.loginView, name='login'),
     
 
 ]
