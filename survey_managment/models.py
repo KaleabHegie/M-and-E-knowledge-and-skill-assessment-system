cb47a4e9881b84e037e953de7f3788e5fba4a134
Feven-shi <fevenshibeshi@gmail.com>
2023-10-04 15:29:56 +0300

    models added

--- survey_managment/models.py
+++ survey_managment/models.py
@@ -1,4 +1,72 @@
 from django.db import models
 
 # Create your models here.
+class Survey(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+class Questionnaire(models.Model):
+    name = models.CharField( max_length=100)
+    instruction = models.TextField()
+    created_at = models.DateTimeField( auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+class Catagory(models.Model):
+    name = models.CharField( max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+
+    def __str__(self):
+        return self.name
+    
+class Choice_group(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    
+class Choice(models.Model):
+    name = models.TextField()
+    weight = models.IntegerField()
+    for_choice_group = models.ForeignKey(Choice_group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    
+TYPE_FIELD = [
+        ("Text" ,"Text"),
+        ("Number", "Number"),
+        ("Radio", "Radio"),
+        ("Select", "Select"),
+        ("Multi-Select", "Multi-Select"),
+        ("Text-Area", "Text Area"),
+        ("URL", "URL"),
+        ("Email", "Email"),
+        ("Date", "Date"),
+        ("Rating", "Rating")
+    ]
+    
+class Question(models.Model):
+    title = models.TextField()
+    label = models.CharField(max_length=100)
+    question_type = models.CharField(max_length=100, choices=TYPE_FIELD)
+    for_questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
+    choice_group = models.ForeignKey(Choice_group, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    catagory = models.ForeignKey(Catagory, on_delete=models.CASCADE)
+    has_weight = models.BooleanField()
+    weight = models.IntegerField()
+    allow_doc = models.BooleanField()
+    doc_label = models.TextField()
+
+
+    def __str__(self):
+        return self.title
+
 
