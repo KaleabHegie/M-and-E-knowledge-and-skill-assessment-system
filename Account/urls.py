fbb0ef9229549865305daeab8559c938b0f8f1a9
Betel Tesfu <116803946+Bethel16@users.noreply.github.com>
2023-10-08 19:19:27 -0700

    login template

--- Account/urls.py
+++ Account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 app_name = 'Account'
 
 urlpatterns = [
@@ -10,7 +11,7 @@ urlpatterns = [
     path('view_profile/',views.view_profile, name='view_profile'),
     path('edit_profile/',views.edit_profile, name='edit_profile'),
 
-
+   
 
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
