ad4637ecc631be43644f2db49ba47ed746ab3a87
Benjii400 <benjiyg400@gmail.com>
2023-10-10 10:29:25 +0300

    addToAdmin

--- survey_managment/admin.py
+++ survey_managment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Question)
+
+admin.site.register(Questionnaire)
+
+admin.site.register(Survey)
+
+admin.site.register(Catagory)
+
+admin.site.register(Choice)
+
+admin.site.register(Choice_group)
+
