0b244b6df28b0b369f690434fe520142a06d6f39
Benjii400 <benjiyg400@gmail.com>
2023-10-16 16:27:00 +0300

    BenjisChange

--- survey_managment/forms.py
+++ survey_managment/forms.py
@@ -1,8 +1,31 @@
 from django import forms
-from .models import Question
+from .models import Question , Survey
 
 class QuestionForm(forms.Form):
     class Meta:
         model = Question
         fields = "__all__"    
+
+class QuestionCreateByEdit(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+class SurveyForm(forms.ModelForm):
+    class Meta:
+        model = Survey
+        fields = ['name', 'year']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'year': forms.DateInput(attrs={'class': 'form-control'}),
+        }        
+     
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['title']
+        widgets = {
+            'title': forms.Textarea(attrs={'class': 'form-control'})
+        }
     
--- survey_managment/urls.py
+++ survey_managment/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('questionCreationByType/', views.questionCreationByType, name='questionCreationByType'),
     # path('textQuestion/', views.questionType, name='textQuestion'),
     path('survey/', views.survey, name='survey'),
-    path('chooseSurvey/', views.chooseSurvey, name='chooseSurvey'),
-    path('displayQuestion/', views.displayQuestion, name='displayQuestion'),
+    path('chooseSurvey/<int:id>/', views.chooseSurvey, name='chooseSurvey'),
+    path('displayQuestion/<int:survey_id>/<int:questionnaire_id>/', views.displayQuestion, name='displayQuestion'),
+    path('choose-target/<int:survey_id>/<int:question_id>/', views.chooseTarget, name='chooseTarget'),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
