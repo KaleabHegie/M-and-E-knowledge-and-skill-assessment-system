291d09ae71084de4d33493a7a2264d9815cf5f31
Benjii400 <benjiyg400@gmail.com>
2023-10-17 13:45:05 +0300

    minorChange

--- survey_managment/forms.py
+++ survey_managment/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Category
-from .models import Question , Survey
+from .models import Question , Survey , Questionnaire
 
 class CategoryForm(forms.ModelForm):
     class Meta:
@@ -11,3 +11,29 @@ class CategoryForm(forms.ModelForm):
             'parent': forms.Select(attrs={'class': 'form-control '}),
         } 
     
+class SurveyForm(forms.ModelForm):
+    class Meta:
+        model = Survey
+        fields = ['name', 'year']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'year': forms.DateInput(attrs={'class': 'form-control'}),
+        }    
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class QuestionnaireForm(forms.ModelForm):
+    class Meta:
+        model = Questionnaire
+        fields = ['name', 'instruction']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'instruction': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+        
