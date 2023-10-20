15838205528c89b51d00498f27edc5a39395e6c3
Feven-shi <fevenshibeshi@gmail.com>
2023-10-20 13:40:37 +0300

    dashboard v1

--- survey_managment/views.py
+++ survey_managment/views.py
@@ -48,6 +48,11 @@ def change_password(request):
     return render(request, 'survey_managment/change_password.html', {'form': form})
 
 
+def index2(request):
+    return render(request, 'index2.html')
+
+
+
 def indexView(request):
     survey_id = request.GET.get('survey_id')
     if survey_id:
@@ -301,11 +306,7 @@ def questionCreationByType(request):
         formID = request.session.get('questionnaire_id')
         ForQuestionnaire = Questionnaire.objects.get(id=formID)
         weightInput = request.POST.get('weightInput')
-<<<<<<< HEAD
         if int(weightInput) > 0:
-=======
-        if weightInput > 0:
->>>>>>> 0d80f9b336572004f9c7a39bdcd5d147d387e72e
             has_weight = True
             weight = weightInput
         else:
