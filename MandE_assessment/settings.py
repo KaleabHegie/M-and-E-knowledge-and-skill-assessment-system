15c12986aaf74fa046ad436689d49b6d377d1fb8
Feven-shi <fevenshibeshi@gmail.com>
2024-02-02 13:53:00 +0300

    m

--- MandE_assessment/settings.py
+++ MandE_assessment/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'survey_managment',
     'Account',
-    'froala_editor',   
 ]
 
 MIDDLEWARE = [
--- MandE_assessment/urls.py
+++ MandE_assessment/urls.py
@@ -8,8 +8,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('survey_managment.urls')) ,
-    path('froala_editor/', include('froala_editor.urls')),
     path('account/', include('Account.urls'))
 
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
