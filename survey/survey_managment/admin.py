65901b82816623cd2497348de5112a3573f338bd
Kaleab <benjiyg400@gmail.com>
2024-03-15 11:34:53 +0300

    Commit

---
+++ survey/survey_managment/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Question)
+admin.site.register(Assesment)
+admin.site.register(Section)
+admin.site.register(Category)
+admin.site.register(Choice)
+admin.site.register(Answer)
+admin.site.register(UserResponse)
+admin.site.register(Department)
+admin.site.register(SurveyType)
+admin.site.register(Document)
+admin.site.register(ContactUs)
+
+
